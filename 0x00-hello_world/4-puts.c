46f70a961e640bbb5b949fea0683fe1e3a8c7fe9
Oladipo Munirat <oladipomunirat@gmail.com>
2022-07-03 12:56:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/*
-*main - entry point
-*return: 0(success)
+/**
+*main- Entry point
+*
+*Return: 0(Success)
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
