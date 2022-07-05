acdb0f5def7dedee611457b303e945e8cddd83d0
Oladipo Munirat <oladipomunirat@gmail.com>
2022-07-04 21:27:38 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,18 +12,18 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("Last digit of %d is \n", n);
+printf("Last digit of %d is ", n);
 if (n > 5)
 {
-printf("greater than 5 \n");
+printf("greater than 5 ");
 }
 if (n == 0)
 {
-printf("0 \n");
+printf("0 ");
 }
 if (n < 6 && n != 0)
 {
-printf("less than 6 and not 0 \n");
+printf("less than 6 and not 0 ");
 }
 return (0);
 
